971fb15390fb0fe8e7b7494d39d819e987f09f6e
BygonBlod <antonin.hunault@yahoo.fr>
2019-11-09 08:19:46 +0100

    bouton play/pause fonctionne et correction erreurs

--- src/controller/ServletMaMusique.java
+++ src/controller/ServletMaMusique.java
@@ -15,15 +15,30 @@ public class ServletMaMusique extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private AudioMaster       am               = null;
+    int count =0;
 
     protected void service( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
 
         // Initialisation de la musique
         if ( request.getParameter( "boutonPlay" ) != null ) {
-            am = new AudioMaster();
-            am.init();
-            ( new Thread( am ) ).start();
+        	if(count==0) {
+        		am = new AudioMaster();
+                am.init();
+                ( new Thread( am ) ).start();
+                count+=2;
+        	}
+        	else {
+        		if((count&1)==0) {
+        			am.pause();
+        			count++;
+        		}
+        		else {
+        			am.continuer();
+        			count++;
+        		}
+        	}
+            
         }
         this.getServletContext().getRequestDispatcher( "/WEB-INF/MaMusique.jsp" ).forward( request, response );
     }
